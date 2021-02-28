24f8aef2f099f85346c81ac6d7ba9ab0896d9dcd
VikashChoudhary <vikash.choudhary@srijan.net>
2021-02-28 13:19:46 +0530

    Error prone for testing sonar cloud integratin

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/me", auth, async (req, res) => {
+	const test = "Test variable to see the sonarcloud github action pr failure";
 	const user = await User.findById(req.user._id).select("-password");
 	res.send(user);
 });
